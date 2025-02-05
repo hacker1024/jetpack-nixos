67f188634b48278f8aaf34e2258213de47e84b76
hacker1024 <hacker1024@users.sourceforge.net>
2025-02-05 12:02:38 +1100

    Use kernelPackages scope for nvidia-display-driver callPackage

--- overlay.nix
+++ overlay.nix
@@ -99,7 +99,7 @@ in
     tests = prev.callPackages ./pkgs/tests { inherit l4tVersion; };
 
     kernelPackagesOverlay = final: prev: {
-      nvidia-display-driver = self.callPackage ./kernel/display-driver.nix { };
+      nvidia-display-driver = final.callPackage ./kernel/display-driver.nix { };
     };
 
     kernel = self.callPackage ./kernel { kernelPatches = [ ]; };
